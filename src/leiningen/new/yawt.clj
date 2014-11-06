cdff1bdae4650656aa3307d16a420ea11200d1fe
gered <gered@blarg.ca>
2014-11-06 15:15:37 -0500

    slightly nicer output when generating a new project

--- src/leiningen/new/yawt.clj
+++ src/leiningen/new/yawt.clj
@@ -71,7 +71,7 @@
   [["src/{{path}}/db.clj" (render "src/root_ns/db_couchdb.clj" data)]])
 
 (defn make-project! [options data]
-  (main/info "Generating a new YAWT project called" (:name data) "using options:" (str/join ", " options))
+  (main/info (str "Generating a new YAWT project called '" (:name data) "' using options: " (str/join ", " options)))
   (apply ->files data
          (concat
            (get-base-files data)
