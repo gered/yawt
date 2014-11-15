bbd0749d1398a9610ba18ead3e803ab9833be328
gered <gered@blarg.ca>
2014-11-15 12:51:03 -0500

    version 0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yawt/lein-template "0.2.1"
+(defproject yawt/lein-template "0.3"
   :description "Yet Another Web Template. An opinionated Leiningen template to generate a base project for building Clojure web applications."
   :url "https://github.com/gered/yawt"
   :license {:name "MIT License"
